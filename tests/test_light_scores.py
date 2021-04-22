5c92355281b9b1fb60791503727de69aee920e26
freemt <yucongo+fmt@gmail.com>
2021-04-22 10:08:11 +0800

    first commit

--- tests/test_light_scores.py
+++ tests/test_light_scores.py
@@ -1,5 +1,8 @@
-from light_scores import __version__
+from light_scores import light_scores
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_light_scores_sanity():
+    _ = light_scores('x', 'y')
+    assert _.mean() >= 0.0  # 0.0
+
+    assert light_scores('x', 'x').mean() > -0.28  # -0.274
